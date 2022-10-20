6dd1eccf807f036e3cdaa6ab0c1fc32f37b0968b
cat <harry@babychou.me>
2022-10-20 16:48:46 +0200

    CRUD for Tag modifications

---
+++ managetacourse/src/TagEditionView/ScanView.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import { View } from "react-native";
+import { Text } from "react-native-paper";
+
+export default function ScanTagView() {
+  return (
+    <View>
+      <Text>This is the Scan Tag View</Text>
+    </View>
+  );
+}
---
+++ managetacourse/src/TagEditionView/TagEditionView.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { View, Text } from "react-native";
+
+export default function TagHomeView(): JSX.Element {
+  return (
+    <View>
+      <Text>Here is the tag home view</Text>
+    </View>
+  );
+}
