131b0465c788d5596a3f38e5d2018298403c977b
harryh <geekcat@protonmail.com>
2022-10-26 18:10:34 +0200

    Added missing react import

--- managetacourse/src/HomeView.tsx
+++ managetacourse/src/HomeView.tsx
@@ -1,5 +1,6 @@
+import React from "react";
 import { Text } from "react-native-paper";
 
-export default function Home() : JSX.Element {
-    return <Text>Bienvenue sur l'accueil!</Text>
+export default function Home(): JSX.Element {
+  return <Text>Bienvenue sur l'accueil!</Text>;
 }
