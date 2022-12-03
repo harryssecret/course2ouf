70b0b8b99dc7447c0e8dd1d5a1b05a6788a957fa
kity <geekcat@protonmail.com>
2022-12-03 20:25:45 +0100

    started working on csv upload form

--- webapp/src/Controller/ImportController.php
+++ webapp/src/Controller/ImportController.php
@@ -3,16 +3,26 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
+#[Route('/import')]
 class ImportController extends AbstractController
 {
-    #[Route('/import', name: 'app_import', methods: ['GET', 'POST'])]
+    #[Route('/', name: 'app_import', methods: ['GET'])]
     public function index(): Response
     {
         return $this->render('import/index.html.twig', [
             'controller_name' => 'ImportController',
         ]);
+    }
+
+    #[Route('/import', name: 'app_send_import', methods: ['GET', 'POST'])]
+    public function new(Request $request): Response
+    {
+        $studentArray = [];
+
+        return $this->render('import/new.html.twig', []);
     }
 }
