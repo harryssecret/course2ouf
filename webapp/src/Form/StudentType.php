5efb034f9b3da87d442976740b5881c7826fa349
Harry Hauer <harry@babychou.me>
2022-12-15 16:51:13 +0100

    updated form with select gender element

--- webapp/src/Form/StudentType.php
+++ webapp/src/Form/StudentType.php
@@ -4,6 +4,7 @@ namespace App\Form;
 
 use App\Entity\Student;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -14,7 +15,12 @@ class StudentType extends AbstractType
         $builder
             ->add('firstname')
             ->add('lastname')
-            ->add('gender')
+            ->add('gender', ChoiceType::class, [
+                'choices' => [
+                    'Homme' => "Homme",
+                    'Femme' => 'Femme'
+                ]
+            ])
             ->add('Grade');
     }
 
