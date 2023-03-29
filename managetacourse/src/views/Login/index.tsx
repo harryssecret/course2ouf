e21081b80d1884f408a35957d753e2574bbd23de
kity <geekcat@protonmail.com>
2023-03-29 14:54:59 +0200

    added credentials data store

--- managetacourse/src/views/Login/index.tsx
+++ managetacourse/src/views/Login/index.tsx
@@ -3,16 +3,26 @@ import {Button, TextInput} from "react-native-paper";
 import React, {useState} from "react";
 import {API_URL} from "@env";
 
+import * as SecureStore from 'expo-secure-store'
+
+async function saveToken(token: string) {
+  await SecureStore.setItemAsync("token", token)
+}
+
 export default function LoginView() {
     const [username, setUsername] = useState("");
     const [password, setPassword] = useState("");
 
-    const logUserIn = () => {
-        fetch(`${API_URL}/auth`, {
+    const logUserIn = async () => {
+        const res = await fetch(`${API_URL}/auth`, {
             method: "POST",
             headers: {"Accept": "application/json", "Content-Type": "application/json"},
             body: JSON.stringify({username: username, password: password})
         })
+      if (res.status === 200) {
+        const body = await res.json()
+        await saveToken(body.token)
+      }
     }
 
     return (
