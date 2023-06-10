a28580351de41a4f76b9a4168e10ba205540d6f6
kity <geekcat@protonmail.com>
2023-06-10 17:58:04 +0200

    added barcode handling

--- assets/react/controllers/QrCodeScannerComponent.tsx
+++ assets/react/controllers/QrCodeScannerComponent.tsx
@@ -1,45 +1,75 @@
-import React, { useState, useEffect, useRef } from "react";
+import React, {useState, useEffect, useRef, useLayoutEffect} from "react";
 import Quagga from "@ericblade/quagga2";
 import { QuaggaJSResultObject } from "@ericblade/quagga2";
 
 export default function () {
   const [isScannerActive, setIsScannerActive] = useState(false);
-  const [results, setResults] = useState([]);
+  const [results, setResults] = useState<QuaggaJSResultObject[]>([]);
 
   const handleBtnClick = () => setIsScannerActive(!isScannerActive);
+
+  const handleOnDetected = (result: QuaggaJSResultObject) => {setResults([...results, result])}
 
   return (
     <div>
       <p>scanner</p>
 
       <button onClick={handleBtnClick} className="btn-primary btn">
-        {isScannerActive ? "Activer" : "Désactiver"} la caméra
+        {isScannerActive ? "Désactiver" : "Activer"} la caméra
       </button>
+      {
+        isScannerActive && <Scanner isActive={isScannerActive} onDetected={handleOnDetected}/>
+      }
+
+
+      <h1>Résultats</h1>
+      <Results results={results}/>
     </div>
   );
 }
 
 type ScannerProps = {
   isActive: boolean;
-  onDetected: () => void;
+  onDetected: (result: QuaggaJSResultObject) => void;
 };
 
 const Scanner: React.FC<ScannerProps> = ({ isActive, onDetected }) => {
   const scannerRef = useRef<HTMLDivElement>(null);
 
-  const handleProcessed = (result: QuaggaJSResultObject) => {
+  const handleProcessed = (result?: QuaggaJSResultObject) => {
     const drawingCtx = Quagga.canvas.ctx.overlay;
     const drawingCanvas = Quagga.canvas.dom.overlay;
     drawingCtx.font = "24px Arial";
     drawingCtx.fillStyle = "green";
-    result.boxes
-      .filter((box) => box !== result.box)
-      .forEach((box) => {
-        Quagga.ImageDebug.drawPath(box, { x: 0, y: 1 }, drawingCtx, {
-          color: "purple",
-          lineWidth: 2,
-        });
-      });
+    if (result) {
+      console.info("[q] quagga processed : ", result)
+      if (result.boxes) {
+        result.boxes
+          .filter((box) => box !== result.box)
+          .forEach((box) => {
+            Quagga.ImageDebug.drawPath(box, { x: 0, y: 1 }, drawingCtx, {
+              color: "purple",
+              lineWidth: 2,
+            });
+          });
+      }
+      if (result.box) {
+        Quagga.ImageDebug.drawPath(result.box, { x: 0, y: 1 }, drawingCtx, { color: 'blue', lineWidth: 2 });
+      }
+      if (result.codeResult && result.codeResult.code) {
+        // const validated = barcodeValidator(result.codeResult.code);
+        // const validated = validateBarcode(result.codeResult.code);
+        // Quagga.ImageDebug.drawPath(result.line, { x: 'x', y: 'y' }, drawingCtx, { color: validated ? 'green' : 'red', lineWidth: 3 });
+        drawingCtx.font = "24px Arial";
+        // drawingCtx.fillStyle = validated ? 'green' : 'red';
+        // drawingCtx.fillText(`${result.codeResult.code} valid: ${validated}`, 10, 50);
+        drawingCtx.fillText(result.codeResult.code, 10, 20);
+        // if (validated) {
+        //     onDetected(result);
+        // }
+      }
+    }
+
   };
 
   useEffect(() => {
@@ -54,15 +84,31 @@ const Scanner: React.FC<ScannerProps> = ({ isActive, onDetected }) => {
           type: "LiveStream",
           target: scannerRef.current,
         },
+        decoder: {
+          readers: ["code_128_reader"]
+        }
       },
       (err) => {
         Quagga.onProcessed(handleProcessed);
 
-        console.error("Error starting QuaggaJS : ", err)
+        if (err) {
+          throw new Error(`Error starting QuaggaJS : ${err}`)
+        }
+        Quagga.start()
+
+
       }
     );
   });
-  return <div ref={scannerRef}></div>;
+  return <div className={"relative"} ref={scannerRef}>
+    <canvas className={"absolute top-0 border-2 border-green-400 w-[640px] h-[480px] drawingBuffer"} />
+  </div>;
 };
 
-const Results = () => <div></div>;
+const Results = ({results}: {results: QuaggaJSResultObject[]}) => <ul className={"menu bg-base-200 w-56 rounded-box"}>
+  {results.map(result => {
+    return (
+      <li>{result.codeResult.code}</li>
+    )
+  })}
+</ul>;
